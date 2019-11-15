a0f86696328c56db791629d2caf7b9b3ef2699cf
Mario Petričević <mario@petricevic.net>
2019-11-15 09:57:07 +0100

    Adds async and await example.

---
+++ Net.Petricevic.Learnings.CSharp/Net.Petricevic.Learnings.CSharp.AsyncAwait/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Net.Petricevic.Learnings.CSharp.AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            var worker = new Worker();
+            worker.DoWork();
+
+            while (!worker.IsComplete)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ Net.Petricevic.Learnings.CSharp/Net.Petricevic.Learnings.CSharp.AsyncAwait/Worker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Net.Petricevic.Learnings.CSharp.AsyncAwait
+{
+    /// <summary>
+    /// Based on https://www.youtube.com/watch?v=6_GTdR0gBVE
+    /// </summary>
+    internal class Worker
+    {
+        public bool IsComplete { get; private set; }
+
+        public async void DoWork()
+        {
+            IsComplete = false;
+            Console.WriteLine("Doing work.");
+
+            await LongOperation();
+
+            Console.WriteLine("Work completed.");
+
+            IsComplete = true;
+        }
+
+        private Task LongOperation()
+        {
+            return Task.Factory.StartNew(() => 
+            {
+                Console.WriteLine("Working...");
+                Thread.Sleep(2000);
+            });
+        }
+    }
+}
